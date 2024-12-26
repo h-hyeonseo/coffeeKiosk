8a9f9005ae2457107a4f78f67a04325e3d719fce
hhs <oiuyv@naver.com>
2024-12-26 16:52:51 +0900

    test

---
+++ src/test/java/com/study/coffeecontroller/CoffeeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.coffeecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
